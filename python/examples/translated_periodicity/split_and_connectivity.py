4191af177571cc6b9352ef0c10f9d9b8cb8a797d
Paht J <paht.juangphanich@gmail.com>
2022-09-15 13:47:24 -0400

    Update split_and_connectivity.py

--- python/examples/translated_periodicity/split_and_connectivity.py
+++ python/examples/translated_periodicity/split_and_connectivity.py
@@ -9,6 +9,11 @@ if not os.path.exists(f'CMC009_connectivity.pickle'):
         [m.pop('match',None) for m in face_matches] # Remove the dataframe
         pickle.dump({"face_matches":face_matches, "outer_faces":outer_faces},f)
 
+with open('CMC009_connectivity.pickle','rb') as f:
+    data = pickle.load(f)
+    face_matches = data['face_matches']
+    outer_faces = data['outer_faces']
+
 periodic_faces, outer_faces, _, _ = translational_periodicity(blocks,face_matches,outer_faces,shift_distance=1.5,shift_direction='z')
 face_matches.extend(periodic_faces)
 periodic_faces, outer_faces, _, _ = translational_periodicity(blocks,face_matches,outer_faces,shift_distance=5.119,shift_direction='y')
