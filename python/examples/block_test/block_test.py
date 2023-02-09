0880e27bcbbb78a40d14bde0f44bcd28f2f30fd2
Paht J <paht.juangphanich@gmail.com>
2023-02-09 16:08:22 -0500

    latest tutorial

--- python/examples/block_test/block_test.py
+++ python/examples/block_test/block_test.py
@@ -51,8 +51,10 @@ dump_data(data)
 data = read_data()
 lower_bound = data['lower_bound']; upper_bound = data['upper_bound']
 left_bound = data['left_bound']; right_bound = data['right_bound']
+x_periodic_faces_export, periodic_faces = translational_periodicity(blocks,left_bound,right_bound,translational_direction='x')
 y_periodic_faces_export, periodic_faces = translational_periodicity(blocks,left_bound,right_bound,translational_direction='y')
 z_periodic_faces_export, periodic_faces = translational_periodicity(blocks,lower_bound,upper_bound,translational_direction='z')
+data['x_periodic'] = z_periodic_faces_export
 data['z_periodic'] = z_periodic_faces_export
 data['y_periodic'] = y_periodic_faces_export
 dump_data(data)
