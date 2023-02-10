8157d2b8d1159435c36646a0124a02a3f5136115
Paht J <paht.juangphanich@gmail.com>
2023-02-10 15:38:37 -0500

    fixed typo error

--- python/examples/translated_periodicity/cmc9.py
+++ python/examples/translated_periodicity/cmc9.py
@@ -34,40 +34,40 @@ if not os.path.exists(f'cmc9_data.pickle'):
     data["connectivity_matrix"]=c
     dump_data(data)
 
-# data = read_data()    
-# all_faces = data['all_faces']
-# connectivity_matrix = data['connectivity_matrix']
+data = read_data()    
+all_faces = data['all_faces']
+connectivity_matrix = data['connectivity_matrix']
 
-# #%% Find bounding Faces
-# lower_bound, upper_bound,_,_ = find_bounding_faces(blocks,connectivity_matrix,all_faces,"z")
-# left_bound, right_bound,_,_ = find_bounding_faces(blocks,connectivity_matrix,all_faces,"y")
-# data['lower_bound'] = lower_bound
-# data['upper_bound'] = upper_bound
-# data['left_bound'] = left_bound
-# data['right_bound'] = right_bound
-# dump_data(data)
+#%% Find bounding Faces
+lower_bound, upper_bound,_,_ = find_bounding_faces(blocks,connectivity_matrix,all_faces,"z")
+left_bound, right_bound,_,_ = find_bounding_faces(blocks,connectivity_matrix,all_faces,"y")
+data['lower_bound'] = lower_bound
+data['upper_bound'] = upper_bound
+data['left_bound'] = left_bound
+data['right_bound'] = right_bound
+dump_data(data)
 
-# #%% Use bounding faces to find periodicity
-# data = read_data()
-# lower_bound = data['lower_bound']; upper_bound = data['upper_bound']
-# left_bound = data['left_bound']; right_bound = data['right_bound']
-# y_periodic_faces_export, periodic_faces = translational_periodicity(blocks,left_bound,right_bound,5.119,translational_direction='y')
-# z_periodic_faces_export, periodic_faces = translational_periodicity(blocks,lower_bound,upper_bound,translational_direction='z')
-# data['z_periodic'] = z_periodic_faces_export
-# data['y_periodic'] = y_periodic_faces_export
-# dump_data(data)
+#%% Use bounding faces to find periodicity
+data = read_data()
+lower_bound = data['lower_bound']; upper_bound = data['upper_bound']
+left_bound = data['left_bound']; right_bound = data['right_bound']
+y_periodic_faces_export, periodic_faces = translational_periodicity(blocks,left_bound,right_bound,5.119,translational_direction='y')
+z_periodic_faces_export, periodic_faces = translational_periodicity(blocks,lower_bound,upper_bound,translational_direction='z')
+data['z_periodic'] = z_periodic_faces_export
+data['y_periodic'] = y_periodic_faces_export
+dump_data(data)
 
-# #%%  Lets check with faces are not periodic in the y-direction
-# data = read_data()
-# y_periodic_faces_export = data['y_periodic']
-# left_bound = data['left_bound']; right_bound = data['right_bound']
+#%%  Lets check with faces are not periodic in the y-direction
+data = read_data()
+y_periodic_faces_export = data['y_periodic']
+left_bound = data['left_bound']; right_bound = data['right_bound']
 
-# left_periodic_blocks_found = [p['block1']['block_index'] for p in y_periodic_faces_export]
-# left_faces_missing = [l for l in left_bound if l['block_index'] not in left_periodic_blocks_found]
-# print('Left faces missing')
-# [print(l) for l in left_faces_missing]
+left_periodic_blocks_found = [p['block1']['block_index'] for p in y_periodic_faces_export]
+left_faces_missing = [l for l in left_bound if l['block_index'] not in left_periodic_blocks_found]
+print('Left faces missing')
+[print(l) for l in left_faces_missing]
 
-# right_periodic_blocks_found = [p['block2']['block_index'] for p in y_periodic_faces_export]
-# right_faces_missing = [r for r in right_bound if r['block_index'] not in right_periodic_blocks_found]
-# print('Right faces missing')
-# [print(r) for r in right_faces_missing]
+right_periodic_blocks_found = [p['block2']['block_index'] for p in y_periodic_faces_export]
+right_faces_missing = [r for r in right_bound if r['block_index'] not in right_periodic_blocks_found]
+print('Right faces missing')
+[print(r) for r in right_faces_missing]
