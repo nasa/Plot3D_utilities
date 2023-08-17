f94cd99eeec774879feb6145a04c39fdfa7a1f88
Paht J <paht.juangphanich@gmail.com>
2023-08-17 09:42:51 -0400

    added docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,46 +1,40 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, '../python')
-import plot3d
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import plot3d
 project = 'Plot3D'
 copyright = '2023, Paht Juangphanich'
 author = 'Paht Juangphanich <paht.juangphanich@nasa.gov>'
+release = '1.6.0'
 
-# The full version, including alpha/beta/rc tags
-version = '1.5.12' # plot3d.__version__
-release = version # plot3d.__version__
-
+rst_context = {'plot3d': plot3d}
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx_copybutton',
-]
+
+html_theme = 'classic'
+html_static_path = ['_static']
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.doctest',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 autosummary_generate = True
 
 napoleon_google_docstring = True
@@ -56,48 +50,6 @@ napoleon_use_param = True
 napoleon_use_rtype = True
 napoleon_use_keyword = True
 napoleon_custom_sections = None
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects htmlstatic_path and html_extra_path.
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
-
-srclink_project = 'https://github.com/nasa/Plot3D_utilities'
-srclink_branch = 'main'
-srclink_src_path = './'
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_theme_options = {
-    'collapse_navigation': False,
-    'display_version': True,
-    'logo_only': True,
-    'navigation_depth': 2,
-}
-
-html_context = {
-    "display_github": True, # Integrate GitHub
-    "github_user": "nasa", # Username
-    "github_repo": "plot3d_utilities", # Repo name
-    "github_version": "main", # Version
-    "conf_py_path": "/docs/", # Path in the checkout to the docs root
-}
-
-html_static_path  = ['_static']
-html_css_files = [
-    'css/style.css',
-]
-rst_context = {'plot3d': plot3d}
 
 def setup(app):
     def skip(app, what, name, obj, skip, options):
