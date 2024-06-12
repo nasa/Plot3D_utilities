4a8aa16d19d759a680d12bd3a15ad3c9f9d892e0
Paht J <paht.juangphanich@gmail.com>
2024-06-12 11:50:59 -0400

    Some major fixes to graph.py
    Fixed bug with connectivity

--- python/examples/metis-grid-splitting/renumber_vertices.py
+++ python/examples/metis-grid-splitting/renumber_vertices.py
@@ -6,6 +6,7 @@ import numpy as np
 import numpy.typing as npt 
 import networkx as nx
 import pickle
+from glennht_con import export_to_glennht_conn
 
 def reindex_vertices(v:npt.NDArray,G:nx.graph.Graph):
     mapping = dict()
@@ -22,6 +23,9 @@ if not os.path.exists('./VSPT_Binary.xyz'):
     periodic_surfaces, outer_faces_to_keep,periodic_faces,outer_faces = periodicity_fast(blocks,outer_faces,face_matches,periodic_direction='k',rotation_axis='x',nblades=55)
     
     face_matches.extend(periodic_surfaces)
+    export_to_glennht_conn(face_matches,outer_faces_to_keep,'vspt')
+
+    
     
     block_sizes = [(b.IMAX, b.JMAX, b.KMAX) for b in blocks]
     graphs = list()
@@ -31,22 +35,26 @@ if not os.path.exists('./VSPT_Binary.xyz'):
         starting_index += b.IMAX*b.JMAX*b.KMAX 
         graphs.append(G1)
     
+    
     G = nx.compose_all(graphs)    
-    G = add_connectivity_to_graph(G,block_sizes,face_matches)
-
+    
     with open('connectivity.pickle','wb') as f:
         [m.pop('match',None) for m in face_matches] # Remove the dataframe
         pickle.dump({
                         "face_matches":face_matches, 
-                        "outer_faces":outer_faces,
-                        "graph":G
+                        "outer_faces":outer_faces_to_keep,
+                        "graph":G,
+                        "block_sizes":block_sizes
                     },f)    
     write_plot3D('VSPT_Binary.xyz',blocks,binary=True)    # Writing plot3D to binary file
-
-else:
-    blocks = read_plot3D('VSPT_Binary.xyz',binary=True)
-    with open('connectivity.pickle','rb') as f:
-        data = pickle.load(f)
-        face_matches = data['face_matches']
-        outer_faces = data['outer_faces']
+    
+# blocks = read_plot3D('VSPT_Binary.xyz',binary=True)
+with open('connectivity.pickle','rb') as f:
+    data = pickle.load(f)
+    face_matches = data['face_matches']
+    outer_faces = data['outer_faces']
+    G = data["graph"]
+    block_sizes = data["block_sizes"]
+    
+G = add_connectivity_to_graph(G,block_sizes,face_matches)
 
