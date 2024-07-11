194b5d0944d90107dce7f777aaf595d7ae29fa5d
Paht J <paht.juangphanich@gmail.com>
2024-07-11 16:51:43 -0400

    Update block_to_graph_4blocktest.py

--- python/examples/metis-block-graph/block_to_graph_4blocktest.py
+++ python/examples/metis-block-graph/block_to_graph_4blocktest.py
@@ -9,3 +9,31 @@ from read_glennht_to_conn import glennht_to_con
 from IPython.display import Image, display
 
 face_matches = []
+
+
+
+# Print number of cells for each part
+for i in range(nparts):
+    indexes = [p for p, e in enumerate(parts) if e == i]
+    nodes = 0 
+    for idx in indexes:  
+        nodes += block_sizes[idx-1]
+    nodes_per_part.append(nodes)
+
+for i in range(nparts):
+    print(f'Parition {i} has {parts.count(i)} blocks')
+
+# Determine work for each partition
+communication_work = np.zeros((nparts,))
+partition_edge_weights = np.zeros((nparts,))
+for b in range(max_block_index+1):
+    partition_id = parts[b]
+    for connected_block in G.adj[b]:
+        connected_block_partition = parts[connected_block]
+        edge_weight = G.adj[b][connected_block]['weight']
+        if connected_block_partition != partition_id:
+            communication_work[partition_id]+=1
+            partition_edge_weights[partition_id] += edge_weight
+
+for i in range(nparts):
+    print(f'Parition {i} com_work {communication_work[i]} edge_work {partition_edge_weights[i]}')
