a83c7225d6113cea2c784e308e29dd0a38e52cc2
Paht J <paht.juangphanich@gmail.com>
2025-01-06 13:53:28 -0500

    trying to fix a dependancy bug

--- python/examples/metis-block-graph/ghtcon_to_ddcmp.py
+++ python/examples/metis-block-graph/ghtcon_to_ddcmp.py
@@ -111,13 +111,13 @@ def read_ght_conn(filename:str):
         face_matches = block_to_block
         return total_blocks, face_matches,num_connections_foreach_block,nZones,zone_types,Zones,GIFs
 
-def create_ddcmp(ght_conn:str,ddcmp_file:str='ddcmp.dat',nparts:int=3,pkl_file_blocksizes:Optional[str]=None):
+def create_ddcmp(ght_conn:str,ddcmp_file:str='ddcmp.dat',nprocessors:int=3,pkl_file_blocksizes:Optional[str]=None):
     """_summary_
 
     Args:
         ght_conn (str): GlennHT Connectivity File
         ddcmp_file (str, optional): Name of the file to write to. Defaults to 'ddcmp.dat'.
-        nparts (int, optional): Number of partitions. Defaults to 3.
+        nprocessors (int, optional): Number of partitions. Defaults to 3.
         pkl_file_blocksizes (Optional[str], optional): Pickle file containing blocksizes. Defaults to None.
     """
     def write_ddcmp(parts:List[int],nZones:int,filename:str='ddcmp.dat'):
@@ -132,44 +132,51 @@ def create_ddcmp(ght_conn:str,ddcmp_file:str='ddcmp.dat',nparts:int=3,pkl_file_b
             np.ndarray: Number of connections between partitions
         """
         nProc = max(parts)+1 # Fortran starts at 1
-        nISP = nZones
+        nISP = nZones # not always true
         nBlocks = len(parts)
         with open(filename,'w') as f:
             f.write(f'{nProc:d}\n')
             f.write(f'{nISP:d}\n') # ISP or number of zones
             f.write(f'{nBlocks:d}\n')
             # Write which block goes to which ISP 
-            for isp in range(nISP):
-                for bIdx in range(nBlocks):
+            for bIdx in range(nBlocks):
+                for isp in range(nISP):
                     f.write(f'{bIdx+1:d} {isp+1:d}\n')
-            # Write which block goes to which Processor 
-            for block_indx,p in enumerate(parts):
-                f.write(f'{block_indx+1:d} {p+1:d}\n')
+            # Write which ISP goes to which Processor 
+            for isp,p in enumerate(parts):
+                f.write(f'{isp+1:d} {p:d}\n')
            
     max_block_index, face_matches,connections,nZones,zone_types,Zones,GIFs = read_ght_conn(ght_conn)
+        
+    assert (nZones==1,"Code is configured to only use single zone (Fluid) or (Solid) not both")
+
     G = block_connectivity_to_graph(face_matches,connections[:,1])
     
     # G.graph['node_weight_attr'] = ['weight']
     G.graph['edge_weight_attr'] = 'weight'
-    (edgecuts, parts) = metis.part_graph(G, nparts,tpwgts=None)
-    
+    (edgecuts, parts) = metis.part_graph(G, nprocessors,tpwgts=None)
+
+    # Single zone, number of ISP = number of processors = keep it simple stupid
+
+    nBlocks = len(parts)
+
     if not pkl_file_blocksizes:
         blocksizes = read_data(pkl_file_blocksizes)         # Useful for knowing how many nodes per partition
         nodes_per_part = list()
         # Print number of cells for each part
-        for i in range(nparts):
+        for i in range(nprocessors):
             indexes = [p for p, e in enumerate(parts) if e == i]
             nodes = 0 
             for idx in indexes:  
                 nodes += blocksizes[idx-1]
             nodes_per_part.append(nodes)
 
-    for i in range(nparts):
+    for i in range(nprocessors):
         print(f'Parition {i} has {parts.count(i)} blocks')
 
     # Determine work for each partition
-    communication_work = np.zeros((nparts,))
-    partition_edge_weights = np.zeros((nparts,))
+    communication_work = np.zeros((nprocessors,))
+    partition_edge_weights = np.zeros((nprocessors,))
     for b in range(1,max_block_index+1):
         partition_id = parts[b]
         for connected_block in G.adj[b]:
@@ -180,15 +187,15 @@ def create_ddcmp(ght_conn:str,ddcmp_file:str='ddcmp.dat',nparts:int=3,pkl_file_b
                 partition_edge_weights[partition_id] += edge_weight
 
     with open('partition.out','w') as f:
-        f.write(f'Number of partitions {nparts}\n')
-        for i in range(nparts):
-            f.write(f'Parition {i} has communication work {communication_work[i]} edge_work {partition_edge_weights[i]}\n')
+        f.write(f'Number of partitions/processors {nprocessors}\n')
+        for i in range(nprocessors):
+            f.write(f'Parition/processor {i} has communication work {communication_work[i]} edge_work {partition_edge_weights[i]}\n')
     
     write_ddcmp(parts[1:],nZones=nZones,filename=ddcmp_file) # Fortran starts at 1 
 
 if __name__=="__main__":
-    create_ddcmp("python/examples/metis-block-graph/CMC9/conn.ght_conn",ddcmp_file='python/examples/metis-block-graph/CMC9/ddcmp.dat',nparts=3,pkl_file_blocksizes='python/examples/metis-block-graph/CMC9/blocksizes.pickle')
-    create_ddcmp("python/examples/metis-block-graph/EEE-Stator/conn.ght_conn",ddcmp_file='python/examples/metis-block-graph/EEE-Stator/ddcmp.dat',nparts=3,pkl_file_blocksizes='python/examples/metis-block-graph/EEE-Stator/blocksizes.pickle')
+    create_ddcmp("python/examples/metis-block-graph/CMC9/conn.ght_conn",ddcmp_file='python/examples/metis-block-graph/CMC9/ddcmp.dat',nprocessors=3,pkl_file_blocksizes='python/examples/metis-block-graph/CMC9/blocksizes.pickle')
+    create_ddcmp("python/examples/metis-block-graph/EEE-Stator/conn.ght_conn",ddcmp_file='python/examples/metis-block-graph/EEE-Stator/ddcmp.dat',nprocessors=3,pkl_file_blocksizes='python/examples/metis-block-graph/EEE-Stator/blocksizes.pickle')
 
 
 
