8e451f78b1a56b2a664ab6b5461e7be96f0b5449
Paht J <paht.juangphanich@gmail.com>
2025-07-15 12:44:45 -0400

    removing stuff that's no longer necessary

--- python/examples/metis-block-graph/block_to_graph_test_large.py
+++ python/examples/metis-block-graph/block_to_graph_test_large.py
@@ -1,7 +1,4 @@
 #%% Import Scripts 
-import sys
-sys.path.insert(0,'../../')
-sys.path.insert(0,'~/miniconda3/envs/dev/lib/python3.10/site-packages/')
 from plot3d.graph import block_connectivity_to_graph
 import numpy as np
 import networkx as nx
